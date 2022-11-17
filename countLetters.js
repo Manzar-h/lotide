d088100f04200b5166f511e1c0febc83f4ae666c
Manzar <manzar.hussain@telus.com>
2022-11-16 21:51:11 -0500

    return a count of each of the letters in the input string

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const result = {};
+  for (let letter of string) {
+    if (letter !== " ") { // Spaces not counted
+      if (result[letter]) { // increasing number for already counted letter
+        result[letter] += 1;
+      } else { // First count of letter
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+console.log(countLetters('LHL'));
+console.log(countLetters(' LLL  HHH  '));
+console.log(countLetters("lighthouse in the house"));
+
+/*
+****Output****
+{ L: 2, H: 1 }
+{ L: 3, H: 3 }
+{ l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 }
+*/
