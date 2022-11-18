d3d8f7969544944d34c6b2244cbb0d29b2b46525
Manzar <manzar.hussain@telus.com>
2022-11-18 01:34:06 -0500

    scan the object and return the first key for which the callback returns a truthy value

--- findKey.js
+++ findKey.js
@@ -8,17 +8,18 @@ const assertEqual = function(actual, expected) {
 
 const findKey = function(object, callback) {
   const keysOfObject = Object.keys(object);//Array of all objects keys
-  let final = "";
+  let final;
   for (let key of keysOfObject) {
     if (callback(object[key])) {
       final = key;
       break; //find the first key and exit the loop
     }
+   
   }
   return final;
 };
 
-
+// first test case: matches 
 assertEqual(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
@@ -28,6 +29,7 @@ assertEqual(findKey({
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 2), "noma");
 
+//second test case: not matches
 assertEqual(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
@@ -37,8 +39,19 @@ assertEqual(findKey({
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 3), "noma");
 
+//3rd test case: no key is found, return undefined.
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4), "noma");
+
 /*
 ****Output***
 ✅✅✅Assertion Passed: noma === noma
 🛑🛑🛑Assertion Failed: Akaleri !== noma
+🛑🛑🛑Assertion Failed: undefined !== noma
 */
