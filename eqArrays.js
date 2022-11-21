71f6f0f0fd9be6b97bac9053534a6a67ec3bfe78
Manzar <manzar.hussain@telus.com>
2022-11-21 18:49:54 -0500

    Lotide Refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
@@ -16,25 +7,5 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
-
-/*
-****output***
-true
-false
-true
-false
-✅✅✅Assertion Passed: true === true
-🛑🛑🛑Assertion Failed: false !== true
-✅✅✅Assertion Passed: true === true
-🛑🛑🛑Assertion Failed: false !== true
-*/
+const assertEqual = require('./assertEqual');
+module.exports = eqArrays;
