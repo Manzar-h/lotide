1b7ee61fdbaadc072175d6d8fcd7efb53ab70880
Manzar <manzar.hussain@telus.com>
2022-11-21 16:30:06 -0500

    Lotide Refactor Example

--- assertEqual.js
+++ assertEqual.js
@@ -6,16 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 3);
-
-/*
-****Output****
-🛑🛑🛑Assertion Failed: Lighthouse Labs !== Bootcamp
-✅✅✅Assertion Passed: Bootcamp === Bootcamp
-✅✅✅Assertion Passed: 1 === 1
-🛑🛑🛑Assertion Failed: 1 !== 3
-*/
+module.exports = assertEqual;
