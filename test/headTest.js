5a94ca102da943942e620180bed27ec6411e6ffb
Manzar <manzar.hussain@telus.com>
2022-11-21 21:48:42 -0500

    Using mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,42 @@
+const assert = require('chai').assert;
+
 const assertEqual = require('../assertEqual');
 const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+});
+
+describe("#head", () => {
+  it("returns Hello for [Hello, Lighthouse, Labs]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
+
+describe("#head", () => {
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(head(['5']), '5'); 
+});
+});
+
+it("returns '8' for ['8']", () => {
+  assert.strictEqual(head(['8']), '8'); 
+});
 
 /*
 ****Output****
-✅✅✅Assertion Passed: 5 === 5
-✅✅✅Assertion Passed: Hello === Hello
+   ✔ returns '8' for ['8']
+  #head
+    ✔ returns 5 for [5, 6, 7]
+
+  #head
+    ✔ returns Hello for [Hello, Lighthouse, Labs]
+
+  #head
+    ✔ returns '5' for ['5']
+
+
+  4 passing (3ms)
 */
