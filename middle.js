66cc0ec9fe72904ba1b020b50de50085c2456ffb
Manzar <manzar.hussain@telus.com>
2022-11-21 20:27:47 -0500

    lotide Refactor middle

--- middle.js
+++ middle.js
@@ -1,29 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function(array) {
   let arr = [];
   
@@ -37,25 +11,5 @@ const middle = function(array) {
   return arr;
 };
 
-//test cases
-console.log(middle([]));
-console.log(middle([1]));
-console.log(middle([1,2]));
+module.exports = middle;
 
-console.log(middle([1,2,3]));
-console.log(middle([1,2,3,4,5]));
-
-console.log(middle([1,2,3,4]));
-console.log(middle([1,2,3,4,5,6]));
-
-
-/*
-****Output****
-[]
-[]
-[]
-[ 2 ]
-[ 3 ]
-[ 2, 3 ]
-[ 3, 4 ]
-*/
